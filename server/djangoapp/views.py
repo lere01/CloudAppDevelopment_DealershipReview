0c9ce69792d16136dd194a6d53fcce6d8e24b17f
Faith Oyedemi <faithoyedemi@gmail.com>
2022-12-25 13:02:34 -0500

    new feautres - login,logout,register

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,6 +30,8 @@ def contact(request):
         return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
+
+
 def login_request(request):
     context = {}
 
@@ -38,19 +40,20 @@ def login_request(request):
         # Get username and password from request.POST dictionary
         username = request.POST['username']
         password = request.POST['psw']
-        
+
         # Try to check if provide credential can be authenticated
         user = authenticate(username=username, password=password)
-        
+
         if user is not None:
             # If user is valid, call login method to login current user
             login(request, user)
-            return redirect('onlinecourse:popular_course_list')
+            return redirect('djangoapp:index')
         else:
             # If not, return to login page again
             return render(request, 'djangoapp/index2.html', context)
     else:
         return render(request, 'djangoapp/index2.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
@@ -58,12 +61,46 @@ def logout_request(request):
     # Logout user in the request
     logout(request)
     # Redirect user back to course list view
-    return render(request, 'djangoapp/index2.html')
+    return redirect('djangoapp:index')
+
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
+    context = {}
+
+    # If it is a GET request, just render the registration page
     if request.method == 'GET':
-        return render(request, 'djangoapp/registration.html')
+        return render(request, 'djangoapp/registration.html', context)
+
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['password']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        user_exist = False
+
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname,
+                                            password=password)
+
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -79,4 +116,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
