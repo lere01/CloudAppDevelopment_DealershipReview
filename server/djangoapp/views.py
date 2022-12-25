a275f18cb81767b7e4598ba5d3e34b1bbf442350
Faith Oyedemi <faithoyedemi@gmail.com>
2022-12-24 20:58:48 -0500

    add more features

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,16 +30,40 @@ def contact(request):
         return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return redirect('onlinecourse:popular_course_list')
+        else:
+            # If not, return to login page again
+            return render(request, 'djangoapp/index2.html', context)
+    else:
+        return render(request, 'djangoapp/index2.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
+    logout(request)
+    # Redirect user back to course list view
+    return render(request, 'djangoapp/index2.html')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
