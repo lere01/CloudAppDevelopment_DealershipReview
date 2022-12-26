9807dc3db95cee0fc347f5158106a970b6754e5d
Faith Oyedemi <faithoyedemi@gmail.com>
2022-12-25 21:11:33 -0500

    create car models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from dataclasses import dataclass
 
 
 # Create your models here.
@@ -9,6 +10,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +27,47 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    dealer_id = models.IntegerField(null=False)
+    car_type = models.CharField(null=False, max_length=50, choices=(
+        ("Sedan", "Sedan"), ("SUV", "SUV"), ("WAGON", "WAGON")))
+    year = models.IntegerField(null=False)
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+@dataclass
+class CarDealer:
+    address: str
+    city: str
+    id: int
+    lat: float
+    long: float
+    st: str
+    state: str
+    zip: str
+
+    def __str__(self) -> str:
+        return f"{self.address}"
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+@dataclass
+class DealerReview:
+    car_make: str
+    car_model: str
+    car_year: int
+    dealership: int
+    id: int
+    name: str
+    purchase: bool
+    purchase_date: str
+    review: str
+
+    def __str__(self) -> str:
+        return f"{self.name}"
